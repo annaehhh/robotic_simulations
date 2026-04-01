a97d3c92d4e582598b8e0acbb451a281fe02bb82
annaehhh <97302590+annaehhh@users.noreply.github.com>
2026-04-01 08:16:24 +0200

    Run existing simulations

---
+++ main.py
@@ -0,0 +1,13 @@
+import irsim
+
+env = irsim.make('ir-sim/usage/17gui_world/gui.yaml') # initialize the environment with the configuration file
+
+for i in range(300): # run the simulation for 300 steps
+
+    env.step()  # update the environment
+    env.render() # render the environment
+
+    if env.done(): break # check if the simulation is done
+
+env.end() # close the environment
+
