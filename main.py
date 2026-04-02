06e5ee34f7882293a12626083cfcf13a46c2cff1
annaehhh <97302590+annaehhh@users.noreply.github.com>
2026-04-02 08:59:39 +0200

    delete test

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-# import irsim
+import irsim
 
-# env = irsim.make('ir-sim/usage/17gui_world/gui.yaml') # initialize the environment with the configuration file
+env = irsim.make('ir-sim/usage/17gui_world/gui.yaml') # initialize the environment with the configuration file
 
-# for i in range(300): # run the simulation for 300 steps
+for i in range(300): # run the simulation for 300 steps
 
-#     env.step()  # update the environment
-#     env.render() # render the environment
+    env.step()  # update the environment
+    env.render() # render the environment
 
-#     if env.done(): break # check if the simulation is done
+    if env.done(): break # check if the simulation is done
 
-# env.end() # close the environment
-
-
-def add_two_numbers(a, b):
-    return a + b
+env.end() # close the environment
 
 
-var_a = 5
-var_b = 10
-
-result = add_two_numbers(var_a, var_b)
-print(f"The result of adding {var_a} and {var_b} is: {result}")
